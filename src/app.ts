6c82c80a70529578a5728983d57a0d2d3dc1f859
IgorCastilhos <igorcastilhos2010@hotmail.com>
2023-09-21 14:22:16 -0300

    Add: First commit, adding prisma, docker config and readme

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Igor Castilhos',
+    email: 'igor@teste.com.br',
+  },
+})
